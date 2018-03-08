e5781685c7cabe40501b9d652201034dff4a110d
OrdinaryMind <1.ordinarymind@gmail.com>
2018-03-08 16:16:12 +0300

    Update Travis config file

--- travis/install_dependencies.sh
+++ travis/install_dependencies.sh
@@ -30,8 +30,8 @@ sudo wget https://github.com/google/googletest/archive/release-1.8.0.tar.gz
 sudo tar xf release-1.8.0.tar.gz
 cd googletest-release-1.8.0/googletest
 ls
-sudo cmake -DBUILD_SHARED_LIBS=ON .
-sudo make
+cmake -DBUILD_SHARED_LIBS=ON .
+make
 sudo cp -a include/gtest /usr/include
 sudo cp -a libgtest_main.so libgtest.so /usr/lib/
 sudo ldconfig -v | grep gtest
