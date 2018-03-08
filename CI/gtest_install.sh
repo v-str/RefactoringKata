b99280a39004fe3f01de6f03d8271c41f415439c
OrdinaryMind <1.ordinarymind@gmail.com>
2018-03-08 13:58:41 +0300

    Update Travic CI config file

--- CI/gtest_install.sh
+++ CI/gtest_install.sh
@@ -3,15 +3,13 @@
 #gtest installation
 
 sudo apt-get -qq update
-sudo apt-get install -y libgtest-dev valgrind cmake
 sudo wget https://github.com/google/googletest/archive/release-1.8.0.tar.gz
 sudo tar xf release-1.8.0.tar.gz
-cd googletest-release-1.8.0
+cd googletest-release-1.8.0/googletest
 sudo cmake -DBUILD_SHARED_LIBS=ON .
 sudo make
 sudo cp -a include/gtest /usr/include
 sudo cp -a libgtest_main.so libgtest.so /usr/lib/
 sudo ldconfig -v | grep gtest
-which valgrind
 cd "${TRAVIS_BUILD_DIR}"
 
