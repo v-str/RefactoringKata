6c2f15f8577ba72b508b417f754033bc0ddfaa71
OrdinaryMind <1.ordinarymind@gmail.com>
2018-03-09 02:20:53 +0300

    Add first test Size Test

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,8 +1,6 @@
 #include <test.h>
 
-TEST(beforeRefactoringTests, test) { EXPECT_EQ(1, 1) << "GTest working test"; }
-
-TEST_F(OriginalTestFixture, initialTest) {
-  EXPECT_EQ(GetPoint().x, 10);
-  EXPECT_EQ(GetPoint().y, 20);
+TEST(FunctionTest, sizeTest) {
+  std::vector<Point> temp_vector;
+  EXPECT_EQ(extractPoints_1(temp_vector).size(), 0);
 }
