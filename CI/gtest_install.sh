32edaf9b18e0153819ddf9a8436475708e35602e
OrdinaryMind <1.ordinarymind@gmail.com>
2018-03-08 03:36:25 +0300

    Rewrite sh Travis script

--- CI/gtest_install.sh
+++ CI/gtest_install.sh
@@ -2,16 +2,13 @@
 
 #gtest installation
 
-echo "BEFORE INSTALLATION"
-
-wget https://github.com/google/googletest/archive/release-1.8.0.tar.gz
-
-tar xf release-1.8.0.tar.gz
+sudo wget https://github.com/google/googletest/archive/release-1.8.0.tar.gz
+sudo tar xf release-1.8.0.tar.gz
 cd googletest-release-1.8.0
-cmake -DBUILD_SHARED_LIBS=ON .
-make
-
+sudo cmake -DBUILD_SHARED_LIBS=ON .
+sudo make
 sudo cp -a include/gtest /usr/include
 sudo cp -a libgtest_main.so libgtest.so /usr/lib/
+g++ -v
 
-echo "AFTER INSTALLATION"
+
