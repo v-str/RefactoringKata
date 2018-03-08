39ef4c73d10b2811228a19699a62c50c020f4dea
OrdinaryMind <1.ordinarymind@gmail.com>
2018-03-08 15:50:33 +0300

    Update Travis config file

--- travis/install_dependencies.sh
+++ travis/install_dependencies.sh
@@ -14,10 +14,10 @@ CMAKE_TAR=cmake-$CMAKE_VERSION-$CMAKE_OS.tar.gz
 CMAKE_URL=http://www.cmake.org/files/$CMAKE_VERSION_DIR/$CMAKE_TAR
 CMAKE_DIR=$(pwd)/cmake-$CMAKE_VERSION
 
-wget --quiet $CMAKE_URL
-mkdir -p $CMAKE_DIR
-tar --strip-components=1 -xzf $CMAKE_TAR -C $CMAKE_DIR
-export PATH=$CMAKE_DIR/bin:$PATH
+sudo wget --quiet $CMAKE_URL
+sudo mkdir -p $CMAKE_DIR
+sudo tar --strip-components=1 -xzf $CMAKE_TAR -C $CMAKE_DIR
+sudo export PATH=$CMAKE_DIR/bin:$PATH
 
 cmake --version
 
