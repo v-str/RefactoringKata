9735c4e63b2fc5f07147ab8d808430c53eaa7587
OrdinaryMind <1.ordinarymind@gmail.com>
2018-03-16 01:30:26 +0300

    Figure out what was wrong and fix it

--- sources/original.cpp
+++ sources/original.cpp
@@ -19,36 +19,13 @@ const std::vector<Point> ExtractFirstPositivePoints(
       std::count_if(Points.begin(), Points.end(),
                     [](const Point &point) { return point.y >= 0; });
 
-  if (amount_of_positive_values == 1 && Points.begin()->y >= 0) {
+  if (Points.begin()->y >= 0 && amount_of_positive_values == 1) {
     return Points;
   }
 
-  unsigned int first_before_negative = 0;
-
-  for (std::size_t i = 0; i < Points.size() - 1; ++i) {
-    if (Points[i].y >= 0 && Points[i + 1].y < 0) {
-      first_before_negative = i;
-      break;
-    }
+  if (Points.begin()->y >= 0 && amount_of_positive_values > 1) {
+    throw std::runtime_error("Unexpected order");
   }
 
-  unsigned int first_after_negative = 0;
-
-  for (std::size_t i = 1; i != Points.size(); i++) {
-    if (Points[i - 1].y < 0 && Points[i].y >= 0) {
-      first_after_negative = i;
-      break;
-    }
-  }
-
-  unsigned int temp = first_before_negative;
-
-  while (temp != first_after_negative) {
-    if (Points[temp].y >= 0) {
-      throw std::runtime_error("Unexpected order");
-    }
-    if (++temp >= Points.size()) temp = 0;  // some magic
-  }
-
-  return std::move(std::vector<Point>());  // move Result
+  return std::move(std::vector<Point>());
 }
