96188f560b2ed66bd75958b0cfaff1656b530288
OrdinaryMind <1.ordinarymind@gmail.com>
2018-03-15 17:07:10 +0300

    Rearrage tests and find right consequence

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -17,54 +17,7 @@ TEST(FunctionTest, SizeTest) {
   EXPECT_EQ(extractPoints_1(temp_vector).size(), 0);
 }
 
-TEST(FunctionTest, GetEmptyVector) {
-  vector<Point> test_vector;
-
-  Point temp_point;
-  temp_point.x = 0;
-
-  for (unsigned int i = kNull; i < kVectorSize; ++i) {
-    if (i == 0 || i == 2) {
-      temp_point.y = -1;
-    } else {
-      temp_point.y = 1;
-    }
-    std::cout << temp_point.y << " ";
-
-    test_vector.push_back(Point(temp_point));
-  }
-
-  std::cout << "\n";
-
-  vector<Point> result_vector = extractPoints_1(test_vector);
-
-  EXPECT_EQ(result_vector.size(), 0);
-}
-
-TEST(FunctionTest, GetEmptyVector2) {
-  vector<Point> test_vector;
-
-  Point temp_point;
-  temp_point.x = 1;
-  temp_point.y = 1;
-
-  for (unsigned int i = kNull; i < kVectorSize; ++i) {
-    if (i == 3 || i == 4 || i == 5) {
-      temp_point.y = 1;
-    } else {
-      temp_point.y = -1;
-    }
-    std::cout << temp_point.y << " ";
-
-    test_vector.push_back(Point(temp_point));
-  }
-
-  std::cout << "\n";
-
-  EXPECT_EQ(extractPoints_1(test_vector).size(), 0);
-}
-
-TEST(FunctionTest, GetEmptyVector3) {
+TEST(FunctionTest, GetEmptyVector1) {
   vector<Point> test_vector;
 
   Point temp_point;
@@ -89,6 +42,53 @@ TEST(FunctionTest, GetEmptyVector3) {
   EXPECT_EQ(result_vector.size(), kNull);
 }
 
+TEST(FunctionTest, GetEmptyVector2) {
+  vector<Point> test_vector;
+
+  Point temp_point;
+  temp_point.x = 0;
+
+  for (unsigned int i = kNull; i < kVectorSize; ++i) {
+    if (i == 0 || i == 1) {
+      temp_point.y = -1;
+    } else {
+      temp_point.y = 1;
+    }
+    std::cout << temp_point.y << " ";
+
+    test_vector.push_back(Point(temp_point));
+  }
+
+  std::cout << "\n";
+
+  vector<Point> result_vector = extractPoints_1(test_vector);
+
+  EXPECT_EQ(result_vector.size(), 0);
+}
+
+TEST(FunctionTest, GetEmptyVector3) {
+  vector<Point> test_vector;
+
+  Point temp_point;
+  temp_point.x = 1;
+  temp_point.y = 1;
+
+  for (unsigned int i = kNull; i < kVectorSize; ++i) {
+    if (i == 0 || i == 1 || i == 2) {
+      temp_point.y = -1;
+    } else {
+      temp_point.y = 1;
+    }
+    std::cout << temp_point.y << " ";
+
+    test_vector.push_back(Point(temp_point));
+  }
+
+  std::cout << "\n";
+
+  EXPECT_EQ(extractPoints_1(test_vector).size(), 0);
+}
+
 TEST(FunctionTest, GetEmptyVector4) {
   vector<Point> test_vector;
 
@@ -97,10 +97,35 @@ TEST(FunctionTest, GetEmptyVector4) {
   temp_point.y = 1;
 
   for (unsigned int i = kNull; i < kVectorSize; ++i) {
-    if (i == 5) {
-      temp_point.y = 1;
-    } else {
+    if (i == 0 || i == 1 || i == 2 || i == 3) {
       temp_point.y = -1;
+    } else {
+      temp_point.y = 1;
+    }
+    std::cout << temp_point.y << " ";
+
+    test_vector.push_back(Point(temp_point));
+  }
+
+  std::cout << "\n";
+
+  vector<Point> result_vector = extractPoints_1(test_vector);
+
+  EXPECT_EQ(result_vector.size(), kNull);
+}
+
+TEST(FunctionTest, GetEmptyVector5) {
+  vector<Point> test_vector;
+
+  Point temp_point;
+  temp_point.x = 1;
+  temp_point.y = 1;
+
+  for (unsigned int i = kNull; i < kVectorSize; ++i) {
+    if (i == 0 || i == 1 || i == 2 || i == 3 || i == 4) {
+      temp_point.y = -1;
+    } else {
+      temp_point.y = 1;
     }
     std::cout << temp_point.y << " ";
 
